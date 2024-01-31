86765c69e99db566630f71f5adf3706875e4f29e
lakshaarora <laksha.arora1@gmail.com>
2024-01-31 12:29:54 -0600

    Laksha - Navbar updated

--- neuro-guide/app/LakshaComps/Comp1.tsx
+++ neuro-guide/app/LakshaComps/Comp1.tsx
@@ -1,15 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
     return (
         <>
-        <div>NeuroGuide</div> 
-        <div>quiz</div> 
-        <div>chat</div> 
-        <div>about us</div> 
-        <div><svg className="h-8 w-8 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div className="px-5 py-8 flex justify-between mx-4 ">
+        <Link href='/' className='font-bold text-xl'>NeuroGuide</Link> 
+        <div className="flex ">
+        <Link href='/' className='px-10 text'>quiz</Link> 
+        <Link href='/' className='px-10 text'>chat</Link> 
+        <Link href='/' className='px-10 text'>about us</Link> 
+        <Link href='/'><svg className="h-6 w-8 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-</svg></div>
+</svg></Link>
+        </div>
+        
+        </div>
+        
         </>  
       )
 }
