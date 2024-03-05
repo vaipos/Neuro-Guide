8a3898104455f07d0295a713a21c2eeebc4e4976
NidhiMajoju <majoju.nidhi@gmail.com>
2024-03-05 11:39:43 -0600

    Fixed the quiz form page - Nidhi

---
+++ neuro-guide/app/NidhiComps/MiniCircle4.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MiniCircle4 = () => {
+  return (
+    <>
+    <div className="rounded-full bg-slate-300 h-2 w-full my-2 mx-3 flex justify-between">
+      <input id="default-radio-31" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-32" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/>   
+      <input id="default-radio-33" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-34" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/>   
+      <input id="default-radio-35" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-36" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/>   
+      <input id="default-radio-37" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-38" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-39" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/> 
+      <input id="default-radio-40" type="radio" value="" name="default-radio-set-4" className="relative left-6 bottom-1 w-28 h-28 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2 focus:ring-offset-gray-100 dark:bg-gray-700 dark:border-gray-600"/>     
+    </div>
+    </>
+  )
+}
+
+export default MiniCircle4
--- neuro-guide/app/NidhiComps/Questions.tsx
+++ neuro-guide/app/NidhiComps/Questions.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import MiniCircles from './MiniCircles1';
 import MiniCircle2 from './MiniCircle2';
 import MiniCircle3 from './MiniCircle3';
+import MiniCircle4 from './MiniCircle4';
+import MiniCircle5 from './MiniCircle5';
+import MiniCircle7 from './MiniCircle7';
+import MiniCircle8 from './MiniCircle8';
+import MiniCircle9 from './MiniCircle9';
+import MiniCircle6 from './MiniCircle6';
+import MiniCircle10 from './MiniCircle10';
+
 
 
 const Questions = () => {
@@ -43,18 +51,7 @@ const Questions = () => {
             <div className="text-left text-3xl">1. Question here</div>
             <div className="flex justify-center my-8">
                 <div className="font-light text-xl">Agree</div>
-                <div className="rounded-full bg-slate-300 h-2 w-full my-2 mx-3 z-0 flex justify-between">
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                </div>
+                    <MiniCircle4/>
                 <div className="font-light text-xl">Disagree</div>
             </div>
         </div>
@@ -64,23 +61,63 @@ const Questions = () => {
             <div className="text-left text-3xl">1. Question here</div>
             <div className="flex justify-center my-8">
                 <div className="font-light text-xl">Agree</div>
-                <div className="rounded-full bg-slate-300 h-2 w-full my-2 mx-3 z-0 flex justify-between">
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                    <MiniCircles/>
-                </div>
+                    <MiniCircle5/>
                 <div className="font-light text-xl">Disagree</div>
             </div>
         </div>
     </div>
-    
+
+    <div className="my-36">
+        <div className="mx-56">
+            <div className="text-left text-3xl">1. Question here</div>
+            <div className="flex justify-center my-8">
+                <div className="font-light text-xl">Agree</div>
+                    <MiniCircle6/>
+                <div className="font-light text-xl">Disagree</div>
+            </div>
+        </div>
+    </div>
+
+    <div className="mb-36">
+        <div className="mx-56">
+            <div className="text-left text-3xl">1. Question here</div>
+            <div className="flex justify-center my-8">
+                <div className="font-light text-xl">Agree</div>
+                    <MiniCircle7/>
+                <div className="font-light text-xl">Disagree</div>
+            </div>
+        </div>
+    </div>
+    <div className="mb-36">
+        <div className="mx-56">
+            <div className="text-left text-3xl">1. Question here</div>
+            <div className="flex justify-center my-8">
+                <div className="font-light text-xl">Agree</div>
+                    <MiniCircle8/>
+                <div className="font-light text-xl">Disagree</div>
+            </div>
+        </div>
+    </div>
+    <div className="mb-36">
+        <div className="mx-56">
+            <div className="text-left text-3xl">1. Question here</div>
+            <div className="flex justify-center my-8">
+                <div className="font-light text-xl">Agree</div>
+                    <MiniCircle9/>
+                <div className="font-light text-xl">Disagree</div>
+            </div>
+        </div>
+    </div>
+    <div className="mb-36">
+        <div className="mx-56">
+            <div className="text-left text-3xl">1. Question here</div>
+            <div className="flex justify-center my-8">
+                <div className="font-light text-xl">Agree</div>
+                    <MiniCircle10/>
+                <div className="font-light text-xl">Disagree</div>
+            </div>
+        </div>
+    </div>
     </>
   )
 }
