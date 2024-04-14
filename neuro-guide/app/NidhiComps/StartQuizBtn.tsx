9074d7d14212619cbf5c0371db8a2f7199228899
ihatepeanuts32 <ntulinaomi32@gmail.com>
2024-04-13 19:18:57 -0500

    Naomi: Take to sign in if they have not signed in yet

--- neuro-guide/app/NidhiComps/StartQuizBtn.tsx
+++ neuro-guide/app/NidhiComps/StartQuizBtn.tsx
@@ -1,14 +1,23 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import NextAuth, { useSession } from 'next-auth/react'
+
 
 
 const StartQuizBtn = () => {
+  
+  const {status, data: session} = useSession();
+
   return (
     <>
       <div className="px-56">
-        <Link href= "/QuizForm" className=" drop-shadow-md bg-white hover:bg-blue-700 transition-colors text-black hover:text-white font-bold py-3 px-44 rounded-2xl mx-80 text-xl">
-        Start
-        </Link> 
+        {status === 'unauthenticated' && <Link href= "/api/auth/signin" className=" drop-shadow-md bg-white hover:bg-blue-700 transition-colors text-black hover:text-white font-bold py-3 px-44 rounded-2xl mx-80 text-xl">
+        Sign in to Start Quiz
+        </Link>  } 
+        {status === 'authenticated' && <Link href= "/QuizForm" className=" drop-shadow-md bg-white hover:bg-blue-700 transition-colors text-black hover:text-white font-bold py-3 px-44 rounded-2xl mx-80 text-xl">
+        Start Quiz
+        </Link>  } 
       </div>
    
     </>
